6fa51a938f248eb71ad0848dabf58e5499f20eb1
Jfeng3 <jfeng1115@gmail.com>
2022-07-03 14:47:03 -0700

    init commit for jupiter analytics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// project import
+import Routes from 'routes';
+import ThemeCustomization from 'themes';
+import Locales from 'components/Locales';
+// import RTLLayout from 'components/RTLLayout';
+import ScrollTop from 'components/ScrollTop';
+import Snackbar from 'components/@extended/Snackbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// auth provider
+import { FirebaseProvider as AuthProvider } from 'contexts/FirebaseContext';
+
+// ==============================|| APP - THEME, ROUTER, LOCAL  ||============================== //
+
+const App = () => (
+  <ThemeCustomization>
+   <Header></Header>
+  </ThemeCustomization>
+);
 
 export default App;
